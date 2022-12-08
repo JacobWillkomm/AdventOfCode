840dd0760948d7b760eb611da3ef8ea057a0e65e
Jacob Willkomm <jacobwillkomm@gmail.com>
2022-12-08 13:08:17 -0800

    day06 solution

--- 2022/day06.js
+++ 2022/day06.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
-let data = fs.readFile("inputs/day01.txt", 'utf8', function (err,data) {
+let data = fs.readFile("inputs/day06.txt", 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
+    let result = data.split('')
+    let substringPacket, substringMessage, packetIndex = 0, messageIndex = 0
 
-    let result = data.split('\n\n').map(ele => ele.split('\n').reduce((sum,ele) => sum+ +ele, 0))
-    result.sort((a,b) => b-a)
-    console.log(result[0], result.slice(0,3).reduce((sum,ele) => sum+ele,0))
+    for(let i = 0; i < result.length; i++){
+      if(i + 4 < result.length){
+        substringPacket = result.slice(i, i+4)
+        if(new Set(substringPacket).size === 4 && packetIndex === 0){
+          packetIndex = i + 4
+        }
+      }
+      if(i + 14 < result.length){
+        substringMessage = result.slice(i, i+14)
+        if(new Set(substringMessage).size === 14 && messageIndex === 0){
+          messageIndex = i + 14
+        }
+      }
+
+    }
+    console.log(packetIndex, messageIndex)
   });
