553327c894dceb51fbe7ae10fce5e4d6510cf3c5
Jacob Willkomm <jacobwillkomm@gmail.com>
2023-01-06 16:20:24 -0800

    day08 part 1

--- 2022/day08.js
+++ 2022/day08.js
@@ -1,4 +1,29 @@
 const fs = require('fs');
+
+const checkVisibility = (grid, row, col) => {
+
+  //edge cases
+  if(row === 0 || row === grid.length-1 || col === 0 || col === grid[0].length-1){
+    return true
+  }
+  let leftArr = grid[row].slice(0, col)
+  let rightArr = grid[row].slice(col+1)
+
+  let verticalArr = grid.map(ele => ele.filter((_, i) => i === col)).flat()
+  let topArr = verticalArr.slice(0, row)
+  let belowArr = verticalArr.slice(row+1)
+
+  if(Math.max(...leftArr) >= grid[row][col]
+  && Math.max(...rightArr) >= grid[row][col]
+  && Math.max(...topArr) >= grid[row][col]
+  && Math.max(...belowArr) >= grid[row][col]){
+    return false
+  }else{
+    return true
+  }
+}
+
+
 let data = fs.readFile("inputs/day08.txt", 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
@@ -9,7 +34,18 @@ let data = fs.readFile("inputs/day08.txt", 'utf8', function (err,data) {
     })
     let rows = grid.length
     let columns = grid[0].length
+    let numVisTrees = 0
 
-    console.log("rows: ", rows, "cols: ", columns)
+    for(let col = 0; col < columns; col++){
+      for(let row = 0; row < rows; row++){
+        //console.log("col: ", col, " row: ", row, grid[col][row], checkVisibility(grid, col, row))
+        if(checkVisibility(grid, col, row)){
+          numVisTrees++
+        }
+      }
+
+    }
+
+    console.log(numVisTrees)
 
   });
