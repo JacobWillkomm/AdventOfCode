c7964a251cd204e2aee709ac6504fae9cb12735e
Jacob Willkomm <jacobwillkomm@gmail.com>
2023-01-16 11:48:36 -0800

    day10 part2 solution

--- 2022/day10.js
+++ 2022/day10.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
+
+//part2 logic
+const drawCrtPixel = (crtRow, signalStrength) => {
+    if(crtRow.length >= signalStrength - 1 && crtRow.length <= signalStrength + 1){
+        crtRow.push('#')
+    }else{
+        crtRow.push('.')
+    }
+    //print and reset crtRow
+    if(crtRow.length == 40){
+        console.log(crtRow.join(''))
+        crtRow = []
+    }
+    return crtRow
+}
+
 let data = fs.readFile("inputs/day10.txt", 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
@@ -8,8 +24,12 @@ let data = fs.readFile("inputs/day10.txt", 'utf8', function (err,data) {
     let targetCycleIndex = 0
     let signalStrength = 1
     let part1SumSignalStrength = 0
+    let crtRow = []
+
     data.trim().split('\n').forEach(ele => {
         let command, value = 0
+
+        //parse input
         if(ele.split(' ').length > 1){
             command = ele.split(' ')[0]
             value = +ele.split(' ')[1]
@@ -17,10 +37,16 @@ let data = fs.readFile("inputs/day10.txt", 'utf8', function (err,data) {
             command = ele
             value = NaN
         }
+
+        //calculate cycle & signal strength
         if(isNaN(value)){
             cycle++
+            crtRow = drawCrtPixel(crtRow, signalStrength)
+            
         }else{
             cycle += 2
+            crtRow = drawCrtPixel(crtRow, signalStrength)
+            crtRow = drawCrtPixel(crtRow, signalStrength)
             signalStrength += +value
         }
 
