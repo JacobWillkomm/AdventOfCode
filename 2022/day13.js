1842469b81727a8e270fd0ef9a8297083e7ccc2c
Jacob Willkomm <jacobwillkomm@gmail.com>
2023-01-23 13:35:21 -0800

    day 13 part2 solution

--- 2022/day13.js
+++ 2022/day13.js
@@ -49,18 +49,35 @@ let data = fs.readFile("inputs/day13.txt", 'utf8', function (err,data) {
     }
 
     let part1Sum = 0
+    let part2Arr = [[[2]],[[6]]]
     data.trim().split('\n\n').forEach((ele,i) => {
-
+        //for each pair of inputs, compare and count
         let leftInput = JSON.parse(ele.split('\n')[0])
         let rightInput = JSON.parse(ele.split('\n')[1])
         let comp = compare(leftInput, rightInput)
         if(comp === 1){
-            console.log(i+1)
             part1Sum += (i + 1)
         }
-
-
+        
+        //create array for part2
+        //we could improve by inserting into the correct spot of a sorted array
+        part2Arr.push(leftInput,rightInput)
     })
+
+    part2Arr.sort((b,a)=>compare(a,b))
+
+    let div1, div2
+    for(let i = 0; i< part2Arr.length; i++){
+        console.log(compare([[2]], part2Arr[i]), compare([[6]], part2Arr[i]))
+        if(compare([[2]], part2Arr[i]) === 0){ 
+            div1 = i + 1
+        }
+        if(compare([[6]], part2Arr[i]) === 0){
+            div2 = i + 1
+        }
+    }
+    console.log(part2Arr)
+    console.log(div1 * div2)
     console.log(part1Sum)
   });
 
